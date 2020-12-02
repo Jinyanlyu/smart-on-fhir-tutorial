c16458c3ce29b1ec0d3fa2fe832a753cd71028e0
Jinyanlyu <42873869+Jinyanlyu@users.noreply.github.com>
2020-12-02 09:04:44 -0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -61,7 +61,7 @@
           //hieght weight
           p.height = getQuantityValueAndUnit(height[0]);
           p.weigth = getQuantityValueAndUnit(weight[0]);
-            p.weight=JSON.stringify(weight[0])
+           // p.weight=JSON.stringify(weight[0])
           //bmi
           p.bmi = (getQuantityValue(weight[0]) / (Math.pow((getQuantityValue(height[0]) / 100), 2))).toFixed(1);
 
@@ -74,7 +74,7 @@
           }
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
-           p.hdl=JSON.stringify(hdl[0])
+          // p.hdl=JSON.stringify(hdl[0])
 
           p.ldl = getQuantityValueAndUnit(ldl[0]);
 
